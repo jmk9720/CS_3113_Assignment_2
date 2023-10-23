0ea41590eef168132550237fd4432d250b084023
jonathan-kai <jonathan@kaistudio.co.kr>
2023-10-22 23:33:06 -0400

    Final FINAL commit

--- CS 3113 Project 2/CS 3113 Project 2/main.cpp
+++ CS 3113 Project 2/CS 3113 Project 2/main.cpp
@@ -8,6 +8,15 @@
 * Academic Misconduct.
 **/
 
+/*
+ Pong Game Variation:
+ This game allows single player and multiplayer modes with multiplayer mode being default.
+ If you press on the key "t", it will switch to single player mode. Item boxes will spawn
+ at pseudo-random locations (so not completely random), and once the ball hits the item,
+ your paddle will increase while decreasing your opponent's paddle. You can only reach a
+ certain size. The ball will speed up and so will your paddle movement. Once the ball hits
+ your opponent side, you win and the game is over.
+ */
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 
